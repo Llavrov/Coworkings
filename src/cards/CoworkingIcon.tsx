e6b32b498f5196ebc97f3f276a2a0e65a5a48928
Llavrov <levlevlavrov@gmail.com>
2023-05-28 19:21:54 +0300

    CoworkingIcon

--- src/cards/CoworkingIcon.tsx
+++ src/cards/CoworkingIcon.tsx
@@ -17,7 +17,7 @@ export enum Icon {
 export default function CoworkingIcon({ icon, edgeLength = 24, className }: CoworkingIconProps): ReactElement {
     return (
         <div className={className} style={{ height: `${edgeLength}px`, width: `${edgeLength}px` }}>
-            <Image src={`${process.env.PUBLIC_URL}${icon}`} width={edgeLength} height={edgeLength} alt={`Coworking icon ${icon}`} />
+            <Image src={icon} width={edgeLength} height={edgeLength} alt={`Coworking icon ${icon}`} />
         </div>
     );
 }
